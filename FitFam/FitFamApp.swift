1ff0b22e5893b1d109737e60abde4cf383bfad3e
JoeHill06 <joehill06@icloud.com>
2025-08-10 16:57:03 +0100

    first commit

---
+++ FitFam/FitFamApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitFamApp.swift
+//  FitFam
+//
+//  Created by Joe Hill on 10/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct FitFamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FitFamTests/FitFamTests.swift
@@ -0,0 +1,17 @@
+//
+//  FitFamTests.swift
+//  FitFamTests
+//
+//  Created by Joe Hill on 10/08/2025.
+//
+
+import Testing
+@testable import FitFam
+
+struct FitFamTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
